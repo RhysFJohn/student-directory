e22f0ce3f64c497347b8e1749d874674945fef07
RhysFJohn <r.freeman-john@outlook.com>
2022-03-03 16:24:47 +0000

    removed hardcoded array of students and implemented a input method so that the user can add students

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
-
 def print_header
   puts "The students of Villains Academy"
   puts "----------------"
@@ -26,11 +10,30 @@ def print(students)
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great student(s)"
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+  
+  while !name.empty? do
+    
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} student(s)"
+
+    name = gets.chomp
+  end
+
+  students
 end
 
 #nothing happens until we call the methods
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
