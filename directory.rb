8d8632fb023f516fc394f89129488e169e76e7e5
RhysFJohn <r.freeman-john@outlook.com>
2022-03-04 14:21:41 +0000

    added ability to save list of students to a csv file and added a gitignore file to ignore csv

--- directory.rb
+++ directory.rb
@@ -28,6 +28,7 @@ def print_menu
   puts "------------------------"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
   puts "------------------------"
   print "Please enter an option: "
@@ -45,12 +46,26 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit #this will terminate the program
   else 
     puts "Invalid option, Please Try again"
   end
 end 
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 def print_header
   puts "The students of Villains Academy"
